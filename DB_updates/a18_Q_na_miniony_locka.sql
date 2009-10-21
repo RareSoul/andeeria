f1b0b1d50eb917dc4016b2d6530291ecee479c83
przemratajczak <przemratajczak@wp.pl>
2009-10-21 22:55:09 +0200

    one more -Binding- quest

--- DB_updates/a18_Q_na_miniony_locka.sql
+++ DB_updates/a18_Q_na_miniony_locka.sql
@@ -4,7 +4,7 @@ DELETE FROM `gameobject` WHERE id =199999;
 INSERT INTO `gameobject` VALUES ('400000','199999','0','1','1','-8971.31','1041.68','52.8626','2.83392','0','0','0.988191','0.153228','25','0','1');
 
 -- Binding (succubus/voidwalker) (horde)
-DELETE FROM spell_script_target WHERE entry IN (9221,9223,9222,9224,8712,8712,7728);
+DELETE FROM spell_script_target WHERE entry IN (9221,9223,9222,9224,8712,8712,7728,8674);
 INSERT INTO spell_script_target VALUES
 -- Summon Voidwalker/Succubus (Orgrimmar & UC)
 (9221,0,37097),
@@ -14,4 +14,6 @@ INSERT INTO spell_script_target VALUES
 -- Summon Felhunter (H/A)
 (8712,0,92388),
 -- Summon Voidwalker (alliance)
-(7728,0,199999);
+(7728,0,199999),
+-- Summon Succubus (alliance)
+(8674,0,199999);
