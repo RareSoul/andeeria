43e0bbe4af86bde8bb577f181713d144b6db81e0
przemratajczak <przemratajczak@wp.pl>
2009-10-28 23:32:25 +0100

    [a33]Q:Leading the Ancestors Hole (Northrend)

--- src/bindings/ScriptDev2/scripts/northrend/borean_tundra.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/borean_tundra.cpp
@@ -22,6 +22,7 @@ SDCategory: Borean Tundra
 EndScriptData */
 
 /* ContentData
+mob_elder
 npc_fizzcrank_fullthrottle
 npc_iruk
 npc_kara_thricestar
@@ -30,6 +31,120 @@ npc_tiare
 EndContentData */
 
 #include "precompiled.h"
+#include "ObjectMgr.h"
+
+/*######
+## mob_elder
+######*/
+enum
+{
+    SPELL_ANCESTOR_RITUAL               = 45536,
+    SPELL_GREEN_BEAM                    = 39165,
+
+    NPC_ELDER_KESUK                     = 25397,
+    NPC_ELDER_SAGANI                    = 25398,
+    NPC_ELDER_TAKRET                    = 25399,
+    
+    GO_ELDER_KESUK                      = 191088,
+    GO_ELDER_SAGANI                     = 191089,
+    GO_ELDER_TAKRET                     = 191090,
+
+    POINT_ID                            = 1
+};
+
+struct MANGOS_DLL_DECL mob_elderAI : public ScriptedAI
+{
+    mob_elderAI(Creature* pCreature) : ScriptedAI(pCreature) { Reset(); }
+
+    bool bEventInProgress;
+    uint64 uiPlayerGUID;
+    uint32 m_uiResetTimer;
+
+    void Reset()
+    {
+        uiPlayerGUID = 0;
+        m_uiResetTimer = 10000;
+
+        m_creature->InterruptSpell(CURRENT_CHANNELED_SPELL,false);
+        m_creature->RemoveAllAuras();
+        m_creature->SetDisplayId(m_creature->GetNativeDisplayId());
+        m_creature->GetMotionMaster()->MoveTargetedHome();
+
+        bEventInProgress = false;
+    }
+
+    void SpellHit(Unit* pCaster, SpellEntry const* pSpell)
+    {
+        if (!pCaster || bEventInProgress)
+            return;
+
+        if (pCaster->GetTypeId() == TYPEID_PLAYER && pSpell->Id == SPELL_ANCESTOR_RITUAL)
+        {
+            bEventInProgress = true;
+            uiPlayerGUID = pCaster->GetGUID();
+            if (CreatureInfo const* pTemp = GetCreatureTemplateStore(m_creature->GetEntry()))
+                m_creature->SetDisplayId(((Player*)pCaster)->GetTeam() == HORDE ? pTemp->DisplayID_H[0] : pTemp->DisplayID_A[0]);
+
+            m_creature->GetMotionMaster()->MoveIdle();
+            m_creature->StopMoving();
+
+            uint32 go_entry;
+            switch (m_creature->GetEntry())
+            {
+                case NPC_ELDER_SAGANI:  go_entry = GO_ELDER_SAGANI; break;
+                case NPC_ELDER_KESUK:   go_entry = GO_ELDER_KESUK; break;
+                case NPC_ELDER_TAKRET:  go_entry = GO_ELDER_TAKRET; break;
+                default: break;
+            }
+            
+            float x,y,z;
+            if (GameObject* pGo = GetClosestGameObjectWithEntry(pCaster, go_entry, DEFAULT_VISIBILITY_DISTANCE))
+                pGo->GetNearPoint(pGo,x,y,z,2.0f,2.0f,pGo->GetOrientation());
+            else 
+            {
+                error_log("SD2: mob_elder (%s entry %u) couldn't find gameobject entry %u. Please check your database",m_creature->GetName(),m_creature->GetEntry(),go_entry);
+                Reset();
+            }
+
+            m_creature->GetMotionMaster()->MovePoint(POINT_ID,x,y,z);
+        }
+    }
+
+    void MovementInform(uint32 type, uint32 id)
+    {
+        if (type != POINT_MOTION_TYPE)
+            return;
+
+        Player* pPlayer = (Player*)Unit::GetUnit((*m_creature),uiPlayerGUID);
+
+        if (!pPlayer)
+            return;
+
+        if (id == POINT_ID)
+        {
+            DoCast(m_creature,SPELL_GREEN_BEAM,false);
+            // Because spell focus is m_creature not static gameobject or something we have to check if player actualy is near m_creature
+            if (pPlayer->GetDistance(m_creature) < 5.0f)
+                pPlayer->KilledMonsterCredit(m_creature->GetEntry(),m_creature->GetGUID()); 
+        }
+    }
+
+    void UpdateAI(const uint32 uiDiff)
+    {
+        if (m_creature->HasAura(SPELL_GREEN_BEAM))
+        {
+            if (m_uiResetTimer <= uiDiff)
+                Reset();
+            else m_uiResetTimer -= uiDiff;
+        }
+    }
+};
+
+CreatureAI* GetAI_mob_elder(Creature* pCreature)
+{
+    return new mob_elderAI(pCreature);
+}
+
 
 /*######
 ## npc_fizzcrank_fullthrottle
@@ -256,6 +371,11 @@ bool GossipSelect_npc_tiare(Player* pPlayer, Creature* pCreature, uint32 uiSende
 void AddSC_borean_tundra()
 {
     Script *newscript;
+
+    newscript = new Script;
+    newscript->Name = "mob_elder";
+    newscript->GetAI = &GetAI_mob_elder;
+    newscript->RegisterSelf();
 
     newscript = new Script;
     newscript->Name = "npc_fizzcrank_fullthrottle";
